5033f9468bf585703e3b8856e33106fc7bbb8af4
Satyabrata <officialsatyabrata@gmail.com>
2021-07-25 21:42:03 +0530

    Updates

---
+++ precache-manifest.b345ab93173efddfbe61cde2b17f3c04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425b41833ede9d7badd23000eb3edecc",
+    "url": "/Task-2/index.html"
+  },
+  {
+    "revision": "91efd565a33554d17448",
+    "url": "/Task-2/static/css/2.0520b0af.chunk.css"
+  },
+  {
+    "revision": "2ac12214d1dabe223d93",
+    "url": "/Task-2/static/css/main.16516185.chunk.css"
+  },
+  {
+    "revision": "91efd565a33554d17448",
+    "url": "/Task-2/static/js/2.a812f74f.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Task-2/static/js/2.a812f74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac12214d1dabe223d93",
+    "url": "/Task-2/static/js/main.00b42269.chunk.js"
+  },
+  {
+    "revision": "0c625da78334f52f45fc",
+    "url": "/Task-2/static/js/runtime-main.ec739cc9.js"
+  },
+  {
+    "revision": "457a038f395fbc0b2db910a98aab26ec",
+    "url": "/Task-2/static/media/1.457a038f.png"
+  },
+  {
+    "revision": "fddb927557e101f62fb6f76d03984ab8",
+    "url": "/Task-2/static/media/11.fddb9275.jpg"
+  },
+  {
+    "revision": "26fdddf6937818b18168138bd360ed80",
+    "url": "/Task-2/static/media/2.26fdddf6.png"
+  },
+  {
+    "revision": "d27e34e45c6ce67c4b54d756d013b9f8",
+    "url": "/Task-2/static/media/ad.d27e34e4.jpg"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/Task-2/static/media/img2.e981619f.svg"
+  }
+]);
